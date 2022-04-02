adb3beca7ddc1a7b2e77045bdc49c54fad613513
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2022-04-01 22:37:35 -0600

    prox

--- index.js
+++ index.js
@@ -4,23 +4,8 @@ const { Gpio } = require('pigpio');
 
 const led1 = new Gpio(22, { mode: Gpio.OUTPUT });
 const led2 = new Gpio(27, { mode: Gpio.OUTPUT });
+const proximitySensor = new Gpio(17, { mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE });
 
-let dutyCycle = 0;
-
-setInterval(() => {
-  led1.pwmWrite(dutyCycle);
-  let inverse = 255 - dutyCycle;
-  inverse = inverse < 0 ? -inverse : inverse;
-  led2.pwmWrite(255 - dutyCycle)
-  dutyCycle += 5;
-  if (dutyCycle > 255) {
-    dutyCycle = 0;
-  }
-}, 20);
-
-process.on('exit', (code) => {
-  console.log('exiting');
-  led1.pwmWrite(0);
-  led2.pwmWrite(0);
-  return code;
-})
+proximitySensor.on('interrupt', (level) => {
+  console.log('level', level);
+});
