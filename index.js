79700b149f1953a0aa47524cee405d3f498900cc
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2022-04-01 22:44:12 -0600

    integration

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const led1 = new Gpio(22, { mode: Gpio.OUTPUT });
 const led2 = new Gpio(27, { mode: Gpio.OUTPUT });
 const proximitySensor = new Gpio(17, { mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE });
 
-proximitySensor.on('interrupt', (level) => {
+
+proximitySensor.watch('interrupt', (level) => {
   console.log('level', level);
+  led1.digitalWrite(level);
+  led2.digitalWrite(level);
+});
+
+process.on('SIGINT', () => {
+  led1.digitalWrite(0);
+  led2.digitalWrite(0);
 });
