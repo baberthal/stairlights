2fbc2e05f15daf079b30bf12662edd229b19e41b
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2022-04-03 19:41:36 -0600

    led better

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const { Gpio } = require('pigpio');
 const LedSet = require('./src/led-set');
 
 // const proximitySensor = new Gpio(17, { mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE });
+const red = new Gpio(16, { mode: Gpio.OUTPUT });
+const green = new Gpio(28, { mode: Gpio.OUTPUT });
+const blue = new Gpio(21, { mode: Gpio.OUTPUT });
+const leds = {
+  red,
+  green,
+  blue,
+};
 
-const leds = new LedSet({
-  redPin: 16,
-  greenPin: 28,
-  bluePin: 21,
-});
-
-leds.setColor(255, 255, 255);
+leds.red.pwmWrite(255);
+leds.green.pwmWrite(255);
+leds.blue.pwmWrite(255);
