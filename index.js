aec93642bb2e581408ca7ccc9e833615609099d0
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2022-04-03 19:46:18 -0600

    led better 3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const LedSet = require('./src/led-set');
 // const proximitySensor = new Gpio(17, { mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE });
 const red = new Gpio(16, { mode: Gpio.OUTPUT });
 const green = new Gpio(20, { mode: Gpio.OUTPUT });
-const blue = new Gpio(21, { mode: Gpio.OUTPUT });
+const blue = new Gpio(12, { mode: Gpio.OUTPUT });
 const leds = {
   red,
   green,
