db4bfbae044b60240e126d439d00f9fef72d65d2
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2022-04-03 19:57:56 -0600

    pulse leds

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const LedSet = require('./src/led-set');
 // const proximitySensor = new Gpio(17, { mode: Gpio.INPUT, edge: Gpio.EITHER_EDGE });
 const red = new Gpio(16, { mode: Gpio.OUTPUT });
 const green = new Gpio(20, { mode: Gpio.OUTPUT });
-const blue = new Gpio(26, { mode: Gpio.OUTPUT });
+const blue = new Gpio(21, { mode: Gpio.OUTPUT });
 const leds = {
   red,
   green,
@@ -17,12 +17,19 @@ leds.red.pwmWrite(255);
 leds.green.pwmWrite(255);
 leds.blue.pwmWrite(255);
 
-for (let i = 0; i < 255; i++) {
-  leds.red.pwmWrite(i);
-  leds.green.pwmWrite(i);
-  leds.blue.pwmWrite(i);
-}
+let dutyCycle = 0;
+let delta = 5;
 
-leds.red.pwmWrite(0);
-leds.green.pwmWrite(0);
-leds.blue.pwmWrite(0);
+setInterval(() => {
+  leds.red.pwmWrite(dutyCycle);
+  leds.green.pwmWrite(dutyCycle);
+  leds.blue.pwmWrite(dutyCycle);
+
+  dutyCycle = dutyCycle + delta;
+  if (dutyCycle > 255) {
+    delta = -5;
+  } else if (dutyCycle <= 0) {
+    delta = 5;
+  }
+}, 20);
+
